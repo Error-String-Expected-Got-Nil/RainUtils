fd0fc449616cb5e8e7d192f1c094a82aca407f74
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-08-16 23:51:28 -0500

    Local Armor
    First version. Verified that the transpiler does insert the instructions properly, but haven't tested that it actually does what I want it to yet.

---
+++ Source/RainUtils/LocalArmor/Patch_ArmorUtility.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace RainUtils.LocalArmor
+{
+    [HarmonyPatch(typeof(ArmorUtility))]
+    public class Patch_ArmorUtility
+    {
+        private static readonly MethodInfo ApplyArmorMethod = AccessTools.Method(typeof(ArmorUtility), 
+            "ApplyArmor");
+
+        private static readonly MethodInfo CheckLocalArmorMethod = AccessTools.Method(typeof(Patch_ArmorUtility),
+            nameof(CheckLocalArmor));
+        
+        [HarmonyPatch(nameof(ArmorUtility.GetPostArmorDamage))]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Patch_GetPostArmorDamage(IEnumerable<CodeInstruction> instructions, 
+            ILGenerator generator)
+        {
+            // When I was doing Rain World modding, tutorials always told you to use this neat thing that BepInEx
+            // modding had called an ILCursor when making a transpiler. It was quite helpful, and I was sure that
+            // Harmony must have an equivalent- and it does, the CodeMatcher. I would have remade it myself if it
+            // didn't already exist. Very handy tool.
+            var cursor = new CodeMatcher(instructions, generator);
+            
+            // Transpiler procedure:
+            // 1 - Seek to end of method
+            // 2 - Find first call of ApplyArmor going backwards. This is usually where natural armor is applied.
+            // 3 - Create label on first instruction afterwards
+            // 4 - Seek to first instruction for loading arguments to the ApplyArmor call
+            // 5 - Insert instructions to load all arguments for CheckLocalArmor and call it
+            // 6 - Insert branch on true to skip the original ApplyArmor call if it was already handled
+            cursor
+                .End() // 1
+                .SearchBackwards(inst => inst.Calls(ApplyArmorMethod)) // 2
+                .Advance(1)
+                .CreateLabel(out var postApplyArmor) // 3
+                .Advance(-12); // 4
+
+            if (cursor.IsValid)
+                return cursor
+                    .Insert(
+                        CodeInstruction.LoadArgument(1, true), // Damage amount
+                        CodeInstruction.LoadArgument(2), // Armor penetration
+                        CodeInstruction.LoadArgument(4, true), // DamageDef
+                        CodeInstruction.LoadArgument(0), // Pawn
+                        CodeInstruction.LoadLocal(6, true), // Metal armor bool
+                        CodeInstruction.LoadArgument(3), // BodyPartRecord
+                        new CodeInstruction(OpCodes.Call, CheckLocalArmorMethod), // 5
+                        new CodeInstruction(OpCodes.Brtrue, postApplyArmor) // 6
+                    )
+                    .Instructions();
+            
+            Log.Error("[RainUtils] - Error in Patch_ArmorUtility transpiler! Invalid cursor position. Using " +
+                      "original method with no changes instead.");
+            return instructions;
+        }
+        
+        // Returns true if there was relevant local armor, false if there was not. Calculates post-armor damage if
+        // there was.
+        public static bool CheckLocalArmor(ref float damageAmount, float armorPen, ref DamageDef damageDef, Pawn pawn, 
+            ref bool metalArmor, BodyPartRecord part)
+        {
+            if (!(pawn.GetComp<ThingComp_LocalArmor>()?.ArmoredParts.TryGetValue(part.def, out var info) ?? false))
+                return false;
+
+            var armorRating = 0f;
+            if (damageDef.armorCategory.armorRatingStat == StatDefOf.ArmorRating_Sharp)
+                armorRating = info.armorSharp;
+            else if (damageDef.armorCategory.armorRatingStat == StatDefOf.ArmorRating_Blunt)
+                armorRating = info.armorBlunt;
+
+            if (armorRating == 0f) return false;
+
+            ApplyArmorMethod.Invoke(null, new object[] { damageAmount, armorPen, armorRating, null, 
+                damageDef, pawn, metalArmor });
+            
+            return true;
+        }
+    }
+}
---
+++ Source/RainUtils/LocalArmor/ThingComp_LocalArmor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+// ReSharper disable InconsistentNaming
+
+namespace RainUtils.LocalArmor
+{
+    public class ThingComp_LocalArmor : ThingComp
+    {
+        public readonly Dictionary<BodyPartDef, LocalArmorInfo> ArmoredParts = 
+            new Dictionary<BodyPartDef, LocalArmorInfo>();
+        
+        public override void Initialize(CompProperties properties)
+        {
+            props = properties;
+
+            foreach (var info in ((CompProperties_LocalArmor)props).localArmorInfo
+                     .Where(info => info.targetPart != null))
+            {
+                // Silently catch any errors for a key already existing since that's already warned about by the
+                // config errors.
+                try
+                {
+                    ArmoredParts.Add(info.targetPart, info);
+                }
+                catch (ArgumentException) { }
+            }
+        }
+    }
+    
+    public class CompProperties_LocalArmor : CompProperties
+    {
+        public List<LocalArmorInfo> localArmorInfo = new List<LocalArmorInfo>();
+
+        public CompProperties_LocalArmor()
+        {
+            compClass = typeof(ThingComp_LocalArmor);
+        }
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            foreach (var configError in base.ConfigErrors(parentDef))
+                yield return configError;
+
+            if (parentDef.thingClass != typeof(Pawn))
+            {
+                yield return parentDef.defName + " has LocalArmor comp but is not a pawn";
+                yield break;
+            }
+            
+            if (localArmorInfo.Empty())
+            {
+                yield return parentDef.defName + " has LocalArmor comp with no defined localArmorInfo";
+                yield break;
+            }
+
+            var seenParts = new List<BodyPartDef>();
+            foreach (var info in localArmorInfo)
+            {
+                if (info.targetPart == null)
+                    yield return parentDef.defName + " has LocalArmor comp with LocalArmorInfo with null targetPart";
+                else if (seenParts.Contains(info.targetPart))
+                    yield return parentDef.defName + " has LocalArmor comp with LocalArmorInfo with targetPart that " +
+                                 "has already defined in a previous LocalArmorInfo";
+                else
+                    seenParts.Add(info.targetPart);
+            }
+        }
+    }
+
+    public class LocalArmorInfo
+    {
+        public BodyPartDef targetPart = null;
+        public float armorSharp = 0f;
+        public float armorBlunt = 0f;
+    }
+}
