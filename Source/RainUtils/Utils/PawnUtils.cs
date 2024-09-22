ade64f3197c94f2e343fc4a4088bb7725781e5c0
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2024-09-22 18:53:10 -0500

    GetJitterer extension method
    Returns a Pawn_DrawTracker's JitterHandler using reflection because for some reason it's a private field

---
+++ Source/RainUtils/Utils/PawnUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace RainUtils.Utils;
+
+public static class PawnUtils
+{
+    private static readonly FieldInfo JittererField = AccessTools.Field(typeof(Pawn_DrawTracker), "jitterer");
+    
+    public static JitterHandler GetJitterer(this Pawn_DrawTracker drawTracker)
+    {
+        return (JitterHandler)JittererField.GetValue(drawTracker);
+    }
+}
